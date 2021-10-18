fda8d935e3f42c70a7742ece5cb0c13291142169
zura-japoshvili <73229102+zura-japoshvili@users.noreply.github.com>
2021-10-18 16:12:05 +0400

    up

---
+++ create.php
@@ -0,0 +1,51 @@
+<?php 
+include_once "include/header.php";
+?>
+<?php 
+    $pdo = new PDO('mysql:host=localhost;dbname=products_crud;','root','');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+
+    echo '<pre>';
+    var_dump($_POST);
+    echo '</pre>';
+
+    $statement = $pdo->prepare("INSERT INTO products (title,image,description,price,create_date)
+    VALUES(:title,:image,:descroption:,:price,:date)");
+
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':image', '');
+    $statement->bindValue(':description', $description);
+    $statement->bindValue(':price', $price);
+    $statement->bindValue(':date', $date('Y-m-d H:i:s'));
+
+    $statement->execute();
+?>
+
+<h1>Create Product</h1>
+<form method="post">
+  <div class="mb-3">
+    <label>Product Image</label><br>
+    <input type="file" name="image">
+  </div>
+  <div class="mb-3">
+    <label>Product Title</label>
+    <input type="text" name="title" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label>Product Description</label>
+    <textarea  name="description" class="form-control"></textarea>
+  </div>
+  <div class="mb-3">
+    <label>Product Price</label>
+    <input type="number" name="price" class="form-control">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+<?php 
+include_once "include/footer.php";
+?>
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+    include_once "include/header.php";
+?>
+<?php 
+    $pdo = new PDO('mysql:host=localhost;dbname=products_crud;','root','');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $statement = $pdo->prepare("SELECT * FROM products");
+    $statement->execute();
+    $products = $statement->fetchAll(PDO::FETCH_ASSOC); 
+?>
+<h1>Products Crud</h1>
+<a href="create.php" type="button" class="btn btn-outline-success">Add Product</a>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Image</th>
+      <th scope="col">Title</th>
+      <th scope="col">Price</th>
+      <th scope="col">Create date</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($products as $i => $product) { ?>
+      <tr>
+      <th scope="row"><?php echo $i +1 ?></th>
+      <td>Mark</td>
+      <td><?php echo $product['title'] ?></td>
+      <td><?php echo $product['price'] ?></td>
+      <td><?php echo $product['create_date'] ?></td>
+      <td>
+        <button type="button" class="btn btn-sm btn-outline-warning">Edit</button>
+        <button type="button" class="btn btn-sm btn-outline-danger">Delete</button>
+      </td>
+    </tr>
+    <?php }?>
+  </tbody>
+</table>
+
+
+<?php 
+    include_once "include/footer.php";
+?>
